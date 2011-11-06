43845451e78f63638247375fcba650b028c69582
Tatu Saloranta <tsaloranta@gmail.com>
2011-11-05 21:46:37 -0700

    Move unit tests as well

---
+++ src/test/java/com/fasterxml/jackson/module/mrbean/TestGenericTypes.java
@@ -0,0 +1,46 @@
+package com.fasterxml.jackson.module.mrbean;
+
+import java.util.*;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class TestGenericTypes
+    extends BaseTest
+{
+    /*
+    /**********************************************************
+    /* Test classes, enums
+    /**********************************************************
+     */
+
+    public interface ListBean {
+        public List<LeafBean> getLeaves();
+    }
+
+    public static class LeafBean {
+        public String value;
+    }
+    
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+
+    /**
+     * Test simple leaf-level bean with 2 implied _beanProperties
+     */
+    public void testSimpleInteface() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new MrBeanModule());
+        ListBean bean = mapper.readValue("{\"leaves\":[{\"value\":\"foo\"}] }", ListBean.class);
+        assertNotNull(bean);
+        List<LeafBean> leaves = bean.getLeaves();
+        assertNotNull(leaves);
+        assertEquals(1, leaves.size());
+        Object ob = leaves.get(0);        
+        assertSame(LeafBean.class, ob.getClass());
+        assertEquals("foo", leaves.get(0).value);
+    }
+}
