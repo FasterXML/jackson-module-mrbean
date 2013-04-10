34adbb7bdbea92fdfadd7c5100b282a5ac499c8b
Tatu Saloranta <tsaloranta@gmail.com>
2013-04-09 17:18:18 -0700

    Upgrade version detection mechanism

--- src/test/java/com/fasterxml/jackson/module/mrbean/TestVersions.java
+++ src/test/java/com/fasterxml/jackson/module/mrbean/TestVersions.java
@@ -8,16 +8,6 @@ import com.fasterxml.jackson.core.Versioned;
  */
 public class TestVersions extends BaseTest
 {
-    /**
-     * Not a good to do this, but has to do, for now...
-     */
-    private final static int MAJOR_VERSION = 2;
-    private final static int MINOR_VERSION = 1;
-
-    // could inject using Maven filters as well...
-    private final static String GROUP_ID = "com.fasterxml.jackson.module";
-    private final static String ARTIFACT_ID = "jackson-module-mrbean";
-    
     public void testMapperVersions()
     {
         assertVersion(new AbstractTypeMaterializer());
@@ -34,12 +24,7 @@ public class TestVersions extends BaseTest
     {
         final Version v = vers.version();
         assertFalse("Should find version information (got "+v+")", v.isUknownVersion());
-        assertEquals(MAJOR_VERSION, v.getMajorVersion());
-        assertEquals(MINOR_VERSION, v.getMinorVersion());
-        // Check patch level initially, comment out for maint versions
-//        assertEquals(0, v.getPatchLevel());
-        assertEquals(GROUP_ID, v.getGroupId());
-        assertEquals(ARTIFACT_ID, v.getArtifactId());
+        assertEquals(PackageVersion.VERSION, v);
     }
 }
 
